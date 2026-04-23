3e43c1adf0370ca99e548954ac865aefe24b754f
Adarshh s kumar <askofficial004@gmail.com>
2026-04-23 15:49:42 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from agent.agent import run_agent
-
-print("AutoStream AI Agent (Offline)")
-
-while True:
-    user = input("You: ")
-    if user.lower() == "exit":
-        break
-    print("Bot:", run_agent(user))
